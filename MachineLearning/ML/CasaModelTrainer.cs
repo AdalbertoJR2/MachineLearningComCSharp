4afea32e2c396f8efd8dcba050ba30e6cc63f391
Adalberto Junior <adalbertopsjunior01@gmail.com>
2026-04-17 14:06:54 -0300

    Implementação - Treinando e salvando o modelo

--- MachineLearning/ML/CasaModelTrainer.cs
+++ MachineLearning/ML/CasaModelTrainer.cs
@@ -8,6 +8,7 @@ public class CasaModelTrainer
 {
     private MLContext mLContext = new MLContext();
     private IDataView dados;
+    private ITransformer modeloTreinado;
     public void CarregarDadosCSV(string path)
     {
         dados = mLContext.Data.LoadFromTextFile<CasaInputData>(
@@ -15,5 +16,24 @@ public class CasaModelTrainer
             hasHeader: true,
             separatorChar: ','
         );
+    }
+
+    public void TreinarModelo()
+    {
+        var pipeline = mLContext.Transforms.Concatenate(
+            "Features",
+            nameof(CasaInputData.Tamanho),
+            nameof(CasaInputData.Quartos)
+        ).Append(mLContext.Regression.Trainers.Sdca(
+            labelColumnName: "Preco",
+            maximumNumberOfIterations: 100
+        ));
+
+        modeloTreinado = pipeline.Fit(dados);
+    }
+
+    public void SalvarModelo(string path)
+    {
+        mLContext.Model.Save(modeloTreinado, dados.Schema, path);
     }
 }
--- MachineLearningConsole/Program.cs
+++ MachineLearningConsole/Program.cs
@@ -6,4 +6,8 @@ void ExemploRegressao()
 {
     var trainer = new CasaModelTrainer();
     trainer.CarregarDadosCSV(Path.Combine(AppContext.BaseDirectory, "casas_treinamento.csv"));
+    trainer.TreinarModelo();
+    
+    var pathModelo = Path.Combine(AppContext.BaseDirectory, "modelo_treinamento_regrassao.zip");
+    trainer.SalvarModelo(pathModelo);
 }
